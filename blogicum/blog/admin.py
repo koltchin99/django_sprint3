7d22939449d629f1073ff824fc09da28da51b692
koltchin99 <koltchin99rzd@yandex.ru>
2025-05-17 14:21:16 +0300

    Начинаем всё сначала

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -2,6 +2,28 @@ from django.contrib import admin
 
 from .models import Category, Location, Post
 
-admin.site.register(Category)
-admin.site.register(Location)
-admin.site.register(Post)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'is_published', 'created_at')
+    list_filter = ('is_published',)
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_published', 'created_at')
+    list_filter = ('is_published',)
+    search_fields = ('name',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'location',
+                    'is_published', 'pub_date')
+    list_filter = ('is_published', 'category', 'location')
+    search_fields = ('title', 'text')
+    date_hierarchy = 'pub_date'
+    filter_horizontal = ()
+    raw_id_fields = ('author',)
